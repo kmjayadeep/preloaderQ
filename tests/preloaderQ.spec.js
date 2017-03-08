6e47c51226156390f9976d5d55275e624842320e
Jayadeep KM <kmjayadeep@gmail.com>
2017-03-08 12:58:41 +0530

    added enqueue tests

--- tests/preloaderQ.spec.js
+++ tests/preloaderQ.spec.js
@@ -1,3 +1,18 @@
 const PreloaderQ = require('../lib/preloaderQ');
-
+const expect = require('chai').expect;
 var preLoader = new PreloaderQ()
+
+describe('Enqueue',function(){
+  describe('Add new task',function(){
+    it('Adds new Tasks',function(){
+      preLoader.enqueueTask(1)
+      preLoader.enqueueTask(2)
+      expect(preLoader.getQueue()).to.deep.equal([1,2])
+    })
+    it('Adding same task gives error',function(){
+      expect(function(){
+        preLoader.enqueueTask(1)
+      }).to.throw(Error)
+    })
+  })
+})
